c1b8d0367f0e110797e179b89a1d66e10b9a07a9
ElenaIlinykh <bengaliki22@gmail.com>
2022-06-30 10:33:45 +0700

    initial commit

---
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.bonus;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class BonusServiceTest {
+
+    @org.junit.jupiter.api.Test
+    @CsvFileSource(resources = "data.csv") //надо эту строку?
+    public void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+}
